87a23df5638fa14a856456871daa3d984f855682
malka liyanage <malka.liyanage96@gmail.com>
2021-01-25 15:33:11 +0530

    ff

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -19,7 +19,7 @@ const start = async () => {
   } catch (err) {
     console.error(err);
   }
-
+  console.log("start.....");
   app.listen(3000, () => {
     console.log('Listening on port 3000!!!!!!!!');
   });
