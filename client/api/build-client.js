e99c864a38894c2c12fc9603bc242917fc21ce2c
malka liyanage <malka.liyanage96@gmail.com>
2021-01-26 09:25:02 +0530

    final

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://www.xxhashanxx-ticketing-app-prod.fun/',
       headers: req.headers,
     });
   } else {
