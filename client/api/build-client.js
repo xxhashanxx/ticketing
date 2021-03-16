6b47e5256d2f9ac09dbb91631642e5387fbe7064
malka liyanage <malka.liyanage96@gmail.com>
2021-03-16 19:07:18 +0530

    try 1

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,8 @@ export default ({ req }) => {
     // We are on the server
     //
     return axios.create({
-      baseURL:
-        'http://www.xxhashanxx-ticketing-app-prod.fun/',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      //baseURL:'http://www.xxhashanxx-ticketing-app-prod.fun/',
       headers: req.headers,
     });
   } else {
@@ -16,3 +16,4 @@ export default ({ req }) => {
     });
   }
 };
+//b6f811f978b0bfa7909a198e03e62763a84d5740ce6987ac164398d7056efcb1
